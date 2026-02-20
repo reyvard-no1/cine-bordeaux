44b5a4ed79142633c9c6c62d08f45c690c68b864
aurelienellena <aurelienellena2612@hotmail.fr>
2026-02-20 15:41:19 +0100

    Fix MovieMailer - remove exit command

--- app/mailers/movie_mailer.rb
+++ app/mailers/movie_mailer.rb
@@ -1,3 +1,13 @@
+class MovieMailer < ApplicationMailer
+  default from: 'notifications@cine-bordeaux.fr'
 
+  def release_notification(user, movie)
+    @user = user
+    @movie = movie
 
-exit 
+    mail(
+      to: @user.email,
+      subject: "🎬 #{@movie.title} est sorti en salle !"
+    )
+  end
+end
